af26c884e8ea7af9070ac80f21a027050c2a90f3
Test <viktorpilevins@gmail.com>
2021-03-25 19:41:07 +0300

    initial project setup

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './style.scss';
+
+const app = (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+)
+
+render(app, document.getElementById('root'));
